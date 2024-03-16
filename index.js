45abd7eceafd87043657b53bfbf9498432be3046
markjake <markjakearzadon@gmail.com>
2024-03-16 22:43:32 +0800

    garbage

---
+++ index.js
@@ -0,0 +1,16 @@
+window.onscroll = () => {
+  let jimmyEatAllThePizza = document.getElementById("jimmy");
+  if (window.scrollY > 0) {
+    jimmyEatAllThePizza.style.backgroundColor = "rgba(59, 59, 59, 0.3)";
+    jimmyEatAllThePizza.style.backdropFilter = "blur(16px)";
+    jimmyEatAllThePizza.style.filter = "saturate(120%) brightness(69%)";
+    jimmyEatAllThePizza.style.backgroundImage = "none";
+  } else {
+    jimmyEatAllThePizza.style.backgroundImage =
+      "linear-gradient(to bottom, #000000dd, transparent)";
+    jimmyEatAllThePizza.style.backgroundColor = "transparent";
+    jimmyEatAllThePizza.style.backdropFilter = "none";
+    jimmyEatAllThePizza.style.filter = "none";
+  }
+  console.log(window.scrollY);
+};
